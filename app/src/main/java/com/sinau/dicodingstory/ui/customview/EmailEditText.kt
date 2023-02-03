8e78fc3e6f26fa3842ac6c9ecce739081064001f
fikrifaizal <fikrifaizal40@gmail.com>
2023-02-03 16:51:31 +0700

    [Add] create custom view (edit text)

---
+++ app/src/main/java/com/sinau/dicodingstory/ui/customview/EmailEditText.kt
@@ -0,0 +1,76 @@
+package com.sinau.dicodingstory.ui.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.util.Patterns
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.sinau.dicodingstory.R
+
+class EmailEditText : AppCompatEditText {
+
+    private lateinit var emailIcon: Drawable
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        hint = R.string.email.toString()
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+
+    private fun init() {
+        emailIcon = ContextCompat.getDrawable(context, R.drawable.ic_email_24) as Drawable
+        compoundDrawablePadding = 12
+        setIconsDrawable(emailIcon)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().isNotEmpty() && !Patterns.EMAIL_ADDRESS.matcher(s)
+                        .matches()
+                ) error = context.getString(R.string.email)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // do nothing
+            }
+        })
+    }
+
+    private fun setIconsDrawable(
+        startOfTheText: Drawable? = null,
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(
+            startOfTheText,
+            topOfTheText,
+            endOfTheText,
+            bottomOfTheText
+        )
+    }
+}
