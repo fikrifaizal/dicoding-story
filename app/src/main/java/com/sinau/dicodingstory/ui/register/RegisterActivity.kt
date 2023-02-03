b3d0c9ce26cd495022e11a83f0f12ae9ff15a4d0
fikrifaizal <fikrifaizal40@gmail.com>
2023-02-04 02:00:43 +0700

    [Update] implementation register

--- app/src/main/java/com/sinau/dicodingstory/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/sinau/dicodingstory/ui/register/RegisterActivity.kt
@@ -1,10 +1,17 @@
 package com.sinau.dicodingstory.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.sinau.dicodingstory.databinding.ActivityRegisterBinding
+import com.sinau.dicodingstory.ui.login.LoginActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
     private val registerViewModel: RegisterViewModel by viewModels()
@@ -14,6 +21,43 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.apply {
+            btnRegister.setOnClickListener {
+                registerHandler()
+            }
+            btnTextLogin.setOnClickListener {
+                val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
         supportActionBar?.hide()
+    }
+
+    private fun registerHandler() {
+        val name = binding.etName.text.toString().trim()
+        val email = binding.etEmail.text.toString().trim()
+        val password = binding.etPassword.text.toString().trim()
+
+        lifecycleScope.launchWhenResumed {
+            launch {
+                registerViewModel.saveUserRegister(name, email, password).collect { result ->
+                    result.onSuccess {
+                        Toast.makeText(this@RegisterActivity, "Register success", Toast.LENGTH_SHORT)
+                            .show()
+
+                        val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+
+                    result.onFailure {
+                        Toast.makeText(this@RegisterActivity, "Register failed", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/sinau/dicodingstory/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/sinau/dicodingstory/ui/register/RegisterViewModel.kt
@@ -1,6 +1,12 @@
 package com.sinau.dicodingstory.ui.register
 
 import androidx.lifecycle.ViewModel
+import com.sinau.dicodingstory.data.AuthRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class RegisterViewModel: ViewModel() {
+@HiltViewModel
+class RegisterViewModel @Inject constructor(private val authRepository: AuthRepository): ViewModel() {
+
+    fun saveUserRegister(name: String, email: String, password: String) = authRepository.saveUserRegister(name, email, password)
 }
