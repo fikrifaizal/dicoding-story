ca73692539572e7bb9db1fc7ce129c0fc73da28d
fikrifaizal <fikrifaizal40@gmail.com>
2023-02-03 16:12:56 +0700

    [Add] create api config and service using retrofit library

---
+++ app/src/main/java/com/sinau/dicodingstory/data/remote/api/ApiConfig.kt
@@ -0,0 +1,32 @@
+package com.sinau.dicodingstory.data.remote.api
+
+import androidx.viewbinding.BuildConfig
+import com.sinau.dicodingstory.BuildConfig as BC
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+    companion object {
+        fun getApiService(): ApiService {
+            val loggingInterceptor = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BC.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/sinau/dicodingstory/data/remote/api/ApiService.kt
@@ -0,0 +1,33 @@
+package com.sinau.dicodingstory.data.remote.api
+
+import retrofit2.http.*
+
+interface ApiService {
+
+    // [require] suspend to enable creating call adapter for response class
+    @FormUrlEncoded
+    @POST("login")
+    suspend fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): LoginResponse
+
+    @FormUrlEncoded
+    @POST("register")
+    suspend fun register(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): RegisterResponse
+
+    @GET("stories")
+    suspend fun getAllStories(
+        @Header("Authorization") token: String
+    ): StoriesResponse
+
+    @GET("stories/{id}")
+    suspend fun getDetailStory(
+        @Path("id") id: String,
+        @Header("Authorization") token: String
+    ): DetailStoryResponse
+}
