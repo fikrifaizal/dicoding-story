1be3fb9e4455b12257387f6b2a4fb33d9386431e
fikrifaizal <fikrifaizal40@gmail.com>
2023-02-04 03:48:30 +0700

    [Update] implementation authetification token from data store

--- app/src/main/java/com/sinau/dicodingstory/ui/splashscreen/SplashscreenActivity.kt
+++ app/src/main/java/com/sinau/dicodingstory/ui/splashscreen/SplashscreenActivity.kt
@@ -1,11 +1,19 @@
 package com.sinau.dicodingstory.ui.splashscreen
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.sinau.dicodingstory.databinding.ActivitySplashscreenBinding
+import com.sinau.dicodingstory.ui.login.LoginActivity
+import com.sinau.dicodingstory.ui.main.MainActivity
+import com.sinau.dicodingstory.ui.main.MainActivity.Companion.EXTRA_TOKEN
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 @SuppressLint("CustomSplashScreen")
 class SplashscreenActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashscreenBinding
@@ -15,5 +23,22 @@ class SplashscreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        lifecycleScope.launchWhenCreated {
+            launch {
+                splashscreenViewModel.getToken().collect { token ->
+                    if (token.isNullOrEmpty()) {
+                        val intent = Intent(this@SplashscreenActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        val intent = Intent(this@SplashscreenActivity, MainActivity::class.java)
+                        intent.putExtra(EXTRA_TOKEN, token)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/sinau/dicodingstory/ui/splashscreen/SplashscreenViewModel.kt
+++ app/src/main/java/com/sinau/dicodingstory/ui/splashscreen/SplashscreenViewModel.kt
@@ -1,6 +1,13 @@
 package com.sinau.dicodingstory.ui.splashscreen
 
 import androidx.lifecycle.ViewModel
+import com.sinau.dicodingstory.data.AuthRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class SplashscreenViewModel: ViewModel() {
+@HiltViewModel
+class SplashscreenViewModel @Inject constructor(private val authRepository: AuthRepository): ViewModel() {
+
+    fun getToken(): Flow<String?> = authRepository.getToken()
 }
