6cab6b7764306c42bd8acd349f0d554707da78ac
fikrifaizal <fikrifaizal40@gmail.com>
2023-02-15 14:49:13 +0700

    [Rename] ketuker gaes namanya

--- app/src/main/java/com/sinau/dicodingstory/di/DataStoreModule.kt
+++ app/src/main/java/com/sinau/dicodingstory/di/DataStoreModule.kt
@@ -1,12 +1,18 @@
 package com.sinau.dicodingstory.di
 
-import com.sinau.dicodingstory.data.remote.api.ApiConfig
-import com.sinau.dicodingstory.data.remote.api.ApiService
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.sinau.dicodingstory.data.local.AuthDataStore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+
+private val Context.dataStore by preferencesDataStore("preferences")
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -14,5 +20,11 @@ object DataStoreModule {
 
     @Singleton
     @Provides
-    fun provideApiService(): ApiService = ApiConfig.getApiService()
+    fun provideDataStore(@ApplicationContext applicationContext: Context): DataStore<Preferences> =
+        applicationContext.dataStore
+
+    @Singleton
+    @Provides
+    fun provideAuthPreference(dataStore: DataStore<Preferences>): AuthDataStore =
+        AuthDataStore(dataStore)
 }
